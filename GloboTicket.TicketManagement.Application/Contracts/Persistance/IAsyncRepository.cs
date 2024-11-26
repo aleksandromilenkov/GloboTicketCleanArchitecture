eb4e1bb8bd2cf2246158fd1acf7bc6af48659bd5
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-11-26 23:22:32 +0100

    domain and application projects

---
+++ GloboTicket.TicketManagement.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T> where T : class
+    {
+        public Task<T> GetByIdAsync(Guid id);
+        public Task<IReadOnlyList<T>> ListAllAsync();
+        public Task<T> AddAsync(T entity);
+        public Task<T> UpdateAsync(T entity);
+        public Task<T> DeleteAsync(T entity);
+    }
+}
