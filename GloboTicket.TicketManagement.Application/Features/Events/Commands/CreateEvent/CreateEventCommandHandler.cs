da0dc03c70d0d9f0cb0e1a7f54eedd529cd328fb
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-12-03 22:48:24 +0100

    creating infrastructure project and implementing email service

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistance;
 using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using System;
@@ -15,10 +17,12 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
-        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper mapper)
+        private readonly IEmailService _emailService;
+        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper mapper, IEmailService emailService)
         {
             _eventRepository = eventRepository;
             _mapper = mapper;
+            _emailService = emailService;
         }
         public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
         {
@@ -27,6 +31,16 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
             var validationResult = await validator.ValidateAsync(request);
             if (validationResult.Errors.Count > 0) throw new Exceptions.ValidationException(validationResult);
             @event = await _eventRepository.AddAsync(@event);
+
+            // Sending email notification to admin address
+            var email = new Email { To = "asd@asd.com", Body = $"A new event was created {request}", Subject = "A new event created" };
+            try
+            {
+                await _emailService.SendEmail(email);
+            }catch(Exception e)
+            {
+
+            }
             return @event.EventId;
         }
     }
