1cef8ddcf90d7ea2d657e2f3695f9a88e7ec9048
Aleksandro Milenkov <aleksandro.macedonia@gmail.com>
2024-12-09 17:28:18 +0100

    integration tests for the Category Controller

---
+++ GloboTicket.TicketManagement.API.IntegrationTests/Base/Utilities.cs
@@ -0,0 +1,65 @@
+﻿using GloboTicket.TicketManagment.Persistence;
+using System;
+using GloboTicket.TicketManagement.Domain.Entities;
+using Microsoft.Extensions.Logging;
+namespace GloboTicket.TicketManagement.API.IntegrationTests.Base
+{
+    public class Utilities
+    {
+        public static void InitializeDbForTests(GloboTicketDbContext context)
+        {
+            var concertGuid = Guid.Parse("{B0788D2F-8003-43C1-92A4-EDC76A7C5DDE}");
+            var musicalGuid = Guid.Parse("{6313179F-7837-473A-A4D5-A5571B43E6A6}");
+            var playGuid = Guid.Parse("{BF3F3002-7E53-441E-8B76-F6280BE284AA}");
+            var conferenceGuid = Guid.Parse("{FE98F549-E790-4E9F-AA16-18C2292A2EE9}");
+            var eventId1 = new Guid();
+            var eventId2 = new Guid();
+
+            context.Categories.Add(new Category
+            {
+                CategoryId = concertGuid,
+                Name = "Concerts",
+                Events = new List<Event>() {
+                    new Event()
+                    {
+                            EventId = eventId1,
+                            Name = "Going to adventure",
+                            Price = 200,
+                            Date = DateTime.Now,
+                            CategoryId = concertGuid
+                        }
+                }
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = musicalGuid,
+                Name = "Musicals",
+                Events = new List<Event>()
+                    {
+                        new Event()
+                        {
+                            EventId = eventId2,
+                            Name = "Going to Asd",
+                            Price = 200,
+                            Date = DateTime.Now,
+                            CategoryId = musicalGuid
+                        }
+                    }
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = playGuid,
+                Name = "Plays",
+                Events = new List<Event>()
+            });
+            context.Categories.Add(new Category
+            {
+                CategoryId = conferenceGuid,
+                Name = "Conferences",
+                Events = new List<Event>()
+            });
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.API.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,56 @@
+﻿using GloboTicket.TicketManagement.API.IntegrationTests.Base;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesList;
+using GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.API.IntegrationTests.Controllers
+{
+    public class CategoryControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public CategoryControllerTests(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResult()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/category/all");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<CategoryListVm>>(responseString);
+
+            Assert.IsType<List<CategoryListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+
+        [Fact]
+        public async Task ReturnsSuccessResultWithEvents()
+        {
+            var client = _factory.GetAnonymousClient();
+
+            var response = await client.GetAsync("/api/category/allwithevents");
+
+            response.EnsureSuccessStatusCode();
+
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var result = JsonSerializer.Deserialize<List<CategoryEventListVm>>(responseString);
+
+            Assert.IsType<List<CategoryEventListVm>>(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
